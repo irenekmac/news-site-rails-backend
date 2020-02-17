a8ba6cd361a3c12d59c4c264b49f5ff0b896e0df
irene macapinlac <irenekaymac@gmail.com>
2020-02-17 16:52:13 +1100

    created seeds and models

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ gem 'coffee-rails', '~> 4.2'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -32,6 +32,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Users
+User.destroy_all
+
+u1 = User.create name:"Luke", email:"luke@ga.co", password_digest:"chicken"
+u2 = User.create name:"John", email:"john@ga.co", password_digest:"chicken"
+u3 = User.create name:"Mary", email:"mary@ga.co", password_digest:"chicken"
+puts "Created #{User.all.length} users"
+
+# Topics
+
+Topic.destroy_all
+
+t1 = name: "Sport"
+t2 = name: "Gossip"
+t3 = name: "Travel"
+puts "Created #{Topic.all.length} topics"
+
+# Articles
+
+Article.destroy_all
+
+t1 = title: "Sport", image:"", description:"", topic_id:
+t2 = name: "Gossip"
+t3 = name: "Travel"
+puts "Created #{Article.all.length} topics"
+
+# ReadList
